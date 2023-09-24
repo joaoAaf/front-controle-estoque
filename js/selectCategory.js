fdc18f032adaf83c5066a674a46eb0e6180a004d
joaoAaf <joao.anderson.af@gmail.com>
2023-09-24 11:15:40 -0300

    atualizada a pagina home e criada a registerEletro

---
+++ js/selectCategory.js
@@ -0,0 +1,25 @@
+function selectCategory() {
+    selected = document.getElementById("inputCategoria").selectedIndex
+    switch (selected) {
+        case 1:
+            window.location.href = ""
+            break
+        case 2:
+            window.location.href = ""
+            break
+        case 3:
+            window.location.href = ""
+            break
+        case 4:
+            window.location.href = ""
+            break
+        case 5:
+            window.location.href = ""
+            break
+        case 6:
+            window.location.href = ""
+            break
+        default:
+            alert("Selecione uma categoria!")
+    }
+}
